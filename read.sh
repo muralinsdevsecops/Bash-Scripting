8644441952ee5c9c8ec3756295add6225565f636
muralins <murali.siddlingegowda@gmail.com>
2025-03-16 20:01:30 +0530

    read  code

---
+++ read.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "This Script will proceed only by taking the input from the user interactively on the terminal while executing the script"
+read -p " Enter the name of the person running the script:" NAME
+echo " name of the user executed the script is $NAME"
