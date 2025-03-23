562ae887f8e70ec08bcf69dfe4b31ef7a50b4286
muralins <murali.siddlingegowda@gmail.com>
2025-03-23 21:31:57 +0530

    Bash Automation

--- pingtesh.sh
+++ pingtesh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-hosts=hosts.txt
 for hostname in `cat hosts.txt`
 
 do 
      `ping -c1 $hostname` &> /dev/null
+      sleep 3
       if [ $? -eq 0 ]
       then
       echo -e "$hostname\t    is ok \n"
